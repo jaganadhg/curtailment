b2a9a4bc4adfb3187e26db92a7bf17b7bdb83fc0
Saima Aman <saimacs@users.noreply.github.com>
2015-01-15 13:47:12 -0800

    Update calculateCurtailment-SCE.R

--- calculateCurtailment-SCE.R
+++ calculateCurtailment-SCE.R
@@ -2,148 +2,105 @@
 # reads DR event dates
 # reads kwh data for all DR events
 # calculates total curtailment for each DR event
-# (uses SCE: socal edison baseline)
+# (uses SCE baseline)
 
-# set parameters
-year =2014
-
-ithDay = "06/17/2014"
-startDR = "13:00"
-endDR = "17:00"
-intStart = "14:00"
-
-startString = paste(ithDay,"-",startDR,sep="")
-intStartString = paste(ithDay,"-",intStart,sep="")
-endString = paste(ithDay,"-",endDR,sep="")
-
-adjustStart = as.numeric("16")
-adjustDur = as.numeric("12")
-
-# set date and timings of the DR day
-startDate = strsplit(startString,"-")[[1]][1]
-startTime = strsplit(startString,"-")[[1]][2]
-endDate = strsplit(endString,"-")[[1]][1]
-endTime = strsplit(endString,"-")[[1]][2]
-targetDate = as.Date(startDate, format="%m/%d/%Y")
-
-intStartDate = strsplit(intStartString,"-")[[1]][1]
-intStartTime = strsplit(intStartString,"-")[[1]][2]
-
-# findbuildings to process
-setwd("/Users/saima/Desktop/Energy Experiments/gcode/nonDRdays/")
-buildlist = list.files(pattern="*.csv")
-buildlist = unlist(strsplit(buildlist,"_"))
-buildlist = sort(buildlist[which((1:length(buildlist)) %% 2 != 0)])
-
-# find list of holidays for each year
-key.val=read.table("config.properties", sep="=", col.names=c("key","value"), as.is=c(1,2))
-holidays = key.val$val[which(key.val$key == "consume.baseline.holidays")]
-holidays = unlist(strsplit(holidays, split=";"))
-holidays = as.Date(holidays, format="%m/%d/%Y")
-
-#------------------------------
-
-timelabels = seq(0,23.75,0.25)
-mm = (timelabels - floor(timelabels))*60
-mm = formatC(mm, width = 2, format = "d", flag = "0")
-hh = floor(timelabels)
-hh = formatC(hh, width = 2, format = "d", flag = "0")
-timelabels = paste(hh, ":", mm, sep="")
-
-startTimeIdx = which(timelabels == startTime) + 1
-endTimeIdx = which(timelabels == endTime)
-intStartTimeIdx = which(timelabels == intStartTime) + 1
-
-DREventLength = endTimeIdx - startTimeIdx + 1
-intEventLength = endTimeIdx - intStartTimeIdx + 1
-
-morningIdxStart = startTimeIdx - adjustStart
-morningIdxEnd = morningIdxStart + (adjustDur - 1)
-morningLength = morningIdxEnd - morningIdxStart + 1    
-
-output1 = array(dim=c(length(buildlist),intEventLength))
-output2 = array(dim=c(length(buildlist),DREventLength))
-
-#-----------------
-
-past10dates = seq( as.Date("2013-01-01"), by=1, len=10)
-past10daysDRdata = array(dim=c(10,DREventLength))
-past10daysMRdata = array(dim=c(10,12))
-buildings = array(dim=c(0,0))
-
-for (indx in 1:length(buildlist)){
+# DR event parameters
+beginDR = 54 # 1:15 PM
+endDR = 69 # 5:00 PM
   
-  cat("building#: ", indx, "\n")
-  filename = paste(buildlist[indx],"_c.csv",sep="")
-  readData = read.csv(filename,header = TRUE, skip = 1)
-  
-  currentPrevDate = targetDate - 1
-  daysFound = 0
-  
-  morningStrLabel = paste(startDate,"-",timelabels[morningIdxStart],sep="")                                                                            
-  morningStpLabel = paste(startDate,"-",timelabels[morningIdxEnd],sep="")                                                                            
-  
-  readStart = which(readData[,1] == morningStrLabel)
-  readEnd = which(readData[,1] == morningStpLabel)
-  
-  validData = FALSE
-  DRMorningData = 0
-  buildings = c(buildings,buildlist[indx])
-  
-  # read data for the DR morning (9:15 am to 12 noon)
-  if (length(readStart) != 0 && length(readEnd) != 0){
-    DRMorningData = readData[readStart:readEnd,3]
-    validData = TRUE  
-  }
+# set FTP data
+username = ""
+pswd = ""
+username = "fmsguest"
+pswd = "gofms!"
+url = paste("ftp://",username,":",pswd,
+            "@fmsdevwin.usc.edu/Files/Electrical_Dashboard/",
+            sep="")
 
-  while (daysFound < 10)
-  {
-    cat("days Found = ", daysFound)
-    cat("current previous date = ", 
-        format(currentPrevDate,format="%m-%d-%Y"), "-------\n")
+# read building codes
+setwd("/Users/saima/Desktop/Energy Experiments/gcode/reducedKWH/")
+bcodes = read.csv("buildingCodes.csv",header=TRUE)
+
+# read event data 
+data12 = read.csv("DRevents2012.csv")
+data13 = read.csv("DRevents2013.csv")
+data14 = read.csv("DRevents2014.csv")
+DRdata = rbind(data12,data13,data14)
+eventDays = unique(DRdata$Date)
+numDays = length(eventDays)
+
+# do for each DR event day
+missing = NULL # DR days skipped
+curtAll = NULL
+eventsAll = NULL
+#for(i in 1:numDays){
+  for(i in 1:5){
     
-    # check if the previous date is not a holiday or weekend
-    if (!(currentPrevDate %in% holidays) &&
-          !grepl("S+",weekdays(currentPrevDate))){
-      
-      kWhDRData = 0
-      kWhMRData = 0
-      
-      readStart = which(readData[,1] == paste(format(currentPrevDate,"%m/%d/%Y"),"-",timelabels[startTimeIdx],sep=""))
-      readEnd = which(readData[,1] == paste(format(currentPrevDate,"%m/%d/%Y"),"-",timelabels[endTimeIdx],sep=""))
-      
-      if (length(readStart) != 0 && length(readEnd) != 0){
-        kWhDRData = readData[readStart:readEnd,3]
-      }
-      readStart = which(readData[,1] == paste(format(currentPrevDate,"%m/%d/%Y"),"-",timelabels[morningIdxStart],sep=""))
-      readEnd = which(readData[,1] == paste(format(currentPrevDate,"%m/%d/%Y"),"-",timelabels[morningIdxEnd],sep=""))
-      
-      if (length(readStart) != 0 && length(readEnd) != 0){
-        kWhMRData = readData[readStart:readEnd,3]
-      }
-      if (!(length(kWhDRData) != DREventLength) && !(length(kWhMRData) != morningLength)){
-        if (!(-1 %in% kWhDRData) && !(-1 %in% kWhMRData)) {     
-          daysFound = daysFound + 1
-          past10dates[daysFound] = as.Date(currentPrevDate)
-          past10daysDRdata[daysFound,] = kWhDRData
-          past10daysMRdata[daysFound,] = kWhMRData
-        }
-      }
-    }
-    currentPrevDate =currentPrevDate - 1
+  cat("i = ", i, "day =", as.character(eventDays[i]),"\n")
+  dataSlice = subset(DRdata, Date==eventDays[i])
+  eventDate = as.Date(dataSlice$Date[1],"%m/%d/%Y")
+  
+  # 1. read observed data from FTP for the event day
+  ipFile = paste(url,"export-",eventDate,".csv",sep="")
+  myDataObs = read.csv(ipFile,header=TRUE,sep=",",as.is=TRUE)
+  
+  # 2. read baseline data from files for the event day
+  ipFile = paste("edison/",eventDate,".csv",sep="")
+  if(file.exists(ipFile)){
+    #readData = try(read.csv(ipFile))
+    myDataBL = read.csv(ipFile,header=TRUE,sep=",",as.is=TRUE)
+  }else{
+    next
   }
   
-  if (validData == TRUE){
-    prev_morning_avg = apply(past10daysMRdata, 2, mean, na.rm = TRUE)
-    adjustfactor = sum(DRMorningData)/sum(prev_morning_avg)
-  }else{
-    adjustfactor <= 1
-  }  
-  unadj_baseline = apply(past10daysDRdata, 2, mean, na.rm = TRUE)
-  output1[indx,] = adjustfactor * unadj_baseline[(length(unadj_baseline)-intEventLength+1):length(unadj_baseline)]
-  output2[indx,] = adjustfactor * unadj_baseline
+  # do for individual buildings on the DR day
+  totalCurtailment = 0
+  numMissed = 0
+  cat("doing buildings: ")
+  for (j in 1:dim(dataSlice)[1]){
+    cat(j, ",")
+    bldng = as.character(dataSlice$Building[j])
+    strategy = as.character(dataSlice$Strategy[j])
+    key = bcodes$Building.Key[which(bcodes$Building.Code == bldng)]
+    kwhIndices = which(myDataObs$szCity == key)
+    
+    # check data for missing values
+    if (length(kwhIndices) < 90){     # when kwh data is missing
+      missed = c(bldng,as.character(eventDate),strategy)
+      missing = rbind(missing,missed) # save missed data info
+      numMissed = numMissed + 1
+      next   # skip for this building; move to next             
+    } 
+    kwh = myDataObs$Total[kwhIndices]
+    # interpolate for missing data
+    kwh = na.fill(kwh, "extend")  
+    # extract kwh during DR
+    kwhDR = kwh[beginDR:endDR]
+    # find BL
+    kwhBL = subset(myDataBL,buildings == bldng)
+    kwhBL = kwhBL[2:17]
+    
+    # calculate curtailment
+    curtailment = sum(kwhBL - kwhDR)
+    #cat(bldng, ":", strategy, ", curt =", curtailment, "\n")
+    totalCurtailment = totalCurtailment + curtailment
+    #cat("total curt =", totalCurtailment, "\n")
+    
+  } # done for each building
+  cat("\n Total curtailment = ", totalCurtailment, "\n")
+  if(numMissed == dim(dataSlice)[1]){
+    next    
+  }
+  # save this events data
+  curtAll = rbind(curtAll,totalCurtailment)
+  eventsAll = rbind(eventsAll,as.character(eventDate))
   
-}
+} # done for each DR event day
 
-my1 = data.frame(buildings,output1)
-write.table(my1, Outputfilepath, row.names = FALSE, col.names = FALSE, quote = FALSE, sep = ",")
+
+
+
+myDF = data.frame(date = eventsAll,
+                  curtailment = curtAll)
+write.csv(myDF,"curtailment-FB.csv")
+write.csv(missing,"missingDRdata.csv",row.names=FALSE)
