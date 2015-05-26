7f0006b3c7c07840fa7d640a18e54d8e6c627610
Saima <saimacs@users.noreply.github.com>
2015-05-26 16:53:13 -0700

    avg errors

--- compareMAPEs.R
+++ compareMAPEs.R
@@ -86,7 +86,7 @@ df = df[-c(16, 24, 25),] #SCC, SCB, LRC
 # add a row of avg error values
 df$building = as.character(df$building)
 rowx = dim(df)[1]
-df[36,] = c("Avg Error",sum(numTestDays),sum(numTrainDays),
+df[rowx+2,] = c("Avg Error",sum(numTestDays),sum(numTrainDays),
             mean(avgError),mean(avgWDerror),mean(avgWMerror),
             mean(avgWSerror),mean(avgKNNerror),mean(avgKNNgError))
 
