a2f7a96fb38fb2e392315f2dbb613b7f168b1cbf
Saima <saimacs@users.noreply.github.com>
2015-05-29 16:16:09 -0700

    edits

--- compareMAPEs.R
+++ compareMAPEs.R
@@ -85,7 +85,7 @@ for (i in 1:numFiles){
   avgEnsLMerror[i] = mean(errors$x)
 }
 
-# 8. find avg ensLM mape for all files
+# 8. find avg ensLM2 mape for all files
 setwd("~/Desktop/curtailment/MAPE/mape-ensLM2/")
 filesEnsLM2 = list.files(pattern="*.csv")
 numFiles = length(filesEnsLM2)
@@ -94,6 +94,17 @@ avgEnsLMerror2 = numeric(numFiles)
 for (i in 1:numFiles){
   errors = read.csv(filesEnsLM2[i])
   avgEnsLMerror2[i] = mean(errors$x)
+}
+
+# 9. find avg ensLM3 mape for all files
+setwd("~/Desktop/curtailment/MAPE/mape-ensLM3/")
+filesEnsLM3 = list.files(pattern="*.csv")
+numFiles = length(filesEnsLM3)
+
+avgEnsLMerror3 = numeric(numFiles)
+for (i in 1:numFiles){
+  errors = read.csv(filesEnsLM3[i])
+  avgEnsLMerror3[i] = mean(errors$x)
 }
 
 # save results 
@@ -107,7 +118,8 @@ df = data.frame(building = substr(filesWD,9,11),
                 KNN = avgKNNerror,
                 KNNglobal = avgKNNgError,
                 EnsLM = avgEnsLMerror,
-                EnsLM2 = avgEnsLMerror2)
+                EnsLM2 = avgEnsLMerror2,
+                EnsLM3 = avgEnsLMerror3)
 
 # leave out spurious data buildings
 df = df[-c(16, 24, 25),] #SCC, SCB, LRC
@@ -119,7 +131,7 @@ rowx = dim(df)[1]
 df[rowx+1,] = c("Avg Error",sum(df$numTestDays),sum(df$numTrainDays),
             mean(df$Histmean),mean(df$WD),mean(df$WM),
             mean(df$WS),mean(df$KNN),mean(df$KNNglobal),
-            mean(df$EnsLM),mean(df$EnsLM2))
+            mean(df$EnsLM),mean(df$EnsLM2),mean(df$EnsLM3))
 
 write.csv(df,"../avg-mapes.csv",row.names=F)
 
