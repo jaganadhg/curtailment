dd55348121b20ea879090b2177fb54eeb1a70fe0
Saima <saimacs@users.noreply.github.com>
2015-05-28 20:05:01 -0700

    ensemble learning

--- ensLM.R
+++ ensLM.R
@@ -1,7 +1,6 @@
 # use linear model to find model weights
 # ensLM
 
-# learn ensemble models from training data
 setwd("~/Desktop/curtailment/Obs/wm/")
 fList = list.files()
 numBds = length(fList)
@@ -9,6 +8,9 @@ numBds = length(fList)
 # do for each building
 for (i in 1:numBds){
   bd = substr(fList[i],1,3)
+  
+  #---------------------------------------
+  #learn ensemble model from training data
   
   # read observed values
   setwd("~/Desktop/curtailment/Obs/wm/")
@@ -45,10 +47,46 @@ for (i in 1:numBds){
     myLM = lm(obs ~ preds1 + preds2 + preds3, data = df)
     LMmodels[[j]] = myLM
   }
-}
-
-#----------------------------------
-# make predictions on test data
-#predict(myLM, newdata = data.frame(preds1 = 49, preds2 = 51, preds3 = 71))
-
-}
+  
+  #---------------------------------------
+  # make predictions for the test data 
+  
+  # read observed data
+  setwd("~/Desktop/curtailment/Obs/test/")
+  inFile = paste(bd,"-obs.csv",sep="")
+  obsTest = read.csv(inFile,header=TRUE,as.is=TRUE)
+  obsTest = obsTest[,2:17]
+  
+   # read WM prediction
+  setwd("~/Desktop/curtailment/Predictions/wm-test/")
+  inFile = paste(bd,"-preds.csv",sep="")
+  predsWMtest = read.csv(inFile,header=TRUE,as.is=TRUE)
+  predsWMtest = predsWMtest[,2:17]
+  
+  # read WS prediction
+  setwd("~/Desktop/curtailment/Predictions/ws-test/")
+  inFile = paste(bd,"-preds.csv",sep="")
+  predsWStest = read.csv(inFile,header=TRUE,as.is=TRUE)
+  predsWStest = predsWStest[,2:17]
+  
+  # read KNN prediction
+  setwd("~/Desktop/curtailment/Predictions/knn-test/")
+  inFile = paste(bd,"-preds.csv",sep="")
+  predsKNNtest = read.csv(inFile,header=TRUE,as.is=TRUE)
+  predsKNNtest = predsKNNtest[,2:17]
+  
+  # do for each interval
+  ensLMpreds = NULL
+  for (j in 1:16){
+    
+    # build a linear model
+    preds = predict(LMmodels[[j]], 
+                  newdata = data.frame(preds1 = predsWMtest[,j],
+                                       preds2 = predsWStest[,j],
+                                       preds3 = predsKNNtest[,j]))
+    ensLMpreds = cbind(ensLMpreds, preds)
+  }
+  
+  # calculate errors
+  
+} # done for each building
