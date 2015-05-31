fc8a601bf7196b8f4e38203c8ca264dca305c185
Saima <saimacs@users.noreply.github.com>
2015-05-31 01:03:48 -0700

    plots

--- plotMAPE3.R
+++ plotMAPE3.R
@@ -3,6 +3,17 @@
 library(ggplot2)
 library(reshape2)
 library(plyr)
+library(lubridate)
+
+# define season limits
+summerStart12 = as.POSIXct("2012-05-16"); # May 16
+summerEnd12 = as.POSIXct("2012-09-1"); # Sep 1
+
+summerStart13 = as.POSIXct("2013-05-16"); # May 16
+summerEnd13 = as.POSIXct("2013-09-1"); # Sep 1
+
+summerStart14 = as.POSIXct("2014-05-16"); # May 16
+summerEnd14 = as.POSIXct("2014-09-1"); # Sep 1
 
 # read obs and pred values
 setwd("~/Desktop/curtailment/Obs/test/")
@@ -10,6 +21,7 @@ fList = list.files(pattern = paste("*.csv"))
 fList = fList [-c(16,24,25,26)]
 numFiles = length(fList)
 
+seasonList = NULL
 dateList = NULL
 dowList = NULL
 bdList = NULL
@@ -23,6 +35,38 @@ for (j in 1:numFiles){
   obs = read.csv(fList[j])  
   DRdates = obs$date
   DRdow = weekdays(as.Date(DRdates))
+  DRyear = year(DRdates)
+  DRdates = as.POSIXct(DRdates)
+  for(k in 1:length(DRyear)){
+    if(DRyear[k] == 2012){
+      if(DRdates[k] < summerStart12){
+        season = "Spring"
+      }else if(DRdates[k] < summerEnd12){
+        season = "Summer"
+      }else{
+        season = "Fall"
+      }
+    }
+    if(DRyear[k] == 2013){
+      if(DRdates[k] < summerStart13){
+        season = "Spring"
+      }else if(DRdates[k] < summerEnd13){
+        season = "Summer"
+      }else{
+        season = "Fall"
+      }
+    }
+    if(DRyear[k] == 2014){
+      if(DRdates[k] < summerStart14){
+        season = "Spring"
+      }else if(DRdates[k] < summerEnd14){
+        season = "Summer"
+      }else{
+        season = "Fall"
+      }
+    }
+    seasonList = c(seasonList,season)
+  }
   obs = obs[,2:17]
   
   # read predicted values HM
@@ -52,9 +96,10 @@ for (j in 1:numFiles){
 df = data.frame(building = bdList,
                 dowList = dowList,
                 dateList = dateList,
+                seasonList = seasonList,
                 mapeHM = mapeHM,
                 mapeEns = mapeEns)
-df1 = melt(df, id=c("building","dowList","dateList"))
+df1 = melt(df, id=c("building","dowList","dateList", "seasonList"))
 
 #------------------------------
 # 1. paired boxplots
@@ -72,3 +117,37 @@ g2
 
 #------------------------------
 #2. DoW boxplots
+
+df1$dowList_f= factor(df1$dowList, levels = 
+                c('Monday','Tuesday','Wednesday','Thursday','Friday'))
+  
+g1 = ggplot(df1) +
+  geom_boxplot(aes(x=variable, y=value,color=variable)) +
+  facet_grid(. ~ dowList_f) + 
+  theme(legend.position = "top")
+
+g2 = g1 + xlab("Model") + ylab("MAPE") +
+  theme(legend.title = element_blank()) + 
+  theme(legend.text = element_text(size = 16)) +
+  theme(axis.title = element_text(size=14)) +
+  theme(axis.text.x = element_blank())
+g2
+
+#------------------------------
+#3. Semester boxplots
+
+df1$seasonList_f= factor(df1$seasonList, levels = 
+                        c('Spring','Summer','Fall'))
+
+g1 = ggplot(df1) +
+  geom_boxplot(aes(x=variable, y=value,color=variable)) +
+  facet_grid(. ~ seasonList_f) + 
+  theme(legend.position = "top")
+
+g2 = g1 + xlab("Model") + ylab("MAPE") +
+  theme(legend.title = element_blank()) + 
+  theme(legend.text = element_text(size = 16)) +
+  theme(axis.title = element_text(size=14)) +
+  theme(axis.text.x = element_blank())
+g2
+
