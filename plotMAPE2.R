f9c37ff0986d14bc17603c640d344f0770f34bcb
Saima <saimacs@users.noreply.github.com>
2015-05-31 23:59:00 -0700

    plots

--- plotMAPE2.R
+++ plotMAPE2.R
@@ -13,8 +13,6 @@ errors = read.csv("avg-mapes.csv", head = TRUE)
 errors = errors[1:32,]
 
 #----------------------------
-#1. scatter plot kwh vs mape
-
 # read DR vectors
 setwd("~/Desktop/curtailment/")
 
@@ -34,6 +32,7 @@ numBuildings = length(allBuildings)
 # do for all buildings
 vectorLength = 199
 avgKWH = NULL
+sdKWH = NULL
 newbd = NULL
 for (j in 1:numBuildings){
   bd = allBuildings[j]
@@ -58,18 +57,22 @@ for (j in 1:numBuildings){
     DRvectors = rbind(DRvectors,vector)
   }
   avgKWH = c(avgKWH,mean(DRvectors[,c(1:96)]))
+  sdKWH = c(sdKWH, sd(DRvectors[,c(1:96)]))
   newbd = c(newbd, bd)
 }  
 avgKWH = avgKWH[-c(16, 24, 25)]
+sdKWH = sdKWH[-c(16, 24, 25)]
 newbd = newbd[-c(16, 24, 25)]
 
 # frame the data
 df = data.frame(building = as.character(errors$building),
                 avgKWH = avgKWH,
+                sdKWH = sdKWH,
                 HistMean = errors$Histmean,
                 EnsRF = errors$EnsRF)
 
-df1 = melt(df, id = c("building","avgKWH"))
+#1. scatter plot kwh vs mape
+df1 = melt(df, id = c("building","avgKWH","sdKWH"))
 g1 = ggplot(df1) + 
   geom_point(size=3,
              aes(avgKWH,value,
@@ -85,9 +88,24 @@ g3 = g2 + theme(legend.text = element_text(size = 16)) +
   theme(axis.text = element_text(size=14))
 g3
 
+# 2. scatter plot sd kwh vs mape
+g1 = ggplot(df1) + 
+  geom_point(size=3,
+             aes(sdKWH,value,
+                 color = variable,
+                 shape = variable)) +
+  geom_smooth(method=lm, aes(x=avgKWH, y=value, color = variable)) 
+g2 = g1 + xlab("Average electricity consumption (in kWH)") + ylab("MAPE") +
+  theme(legend.position = "top") +
+  theme(legend.title = element_blank())
 
-  #-----------------------
-# 4. Paired boxplots
+g3 = g2 + theme(legend.text = element_text(size = 16)) +
+  theme(axis.title = element_text(size=14)) +
+  theme(axis.text = element_text(size=14))
+g3
+
+#-----------------------
+# 5. Paired boxplots
 df1 = melt(df, id="building")
 g1 = ggplot(df1) +
   geom_boxplot(aes(x=building, y=value,color=variable)) + 
